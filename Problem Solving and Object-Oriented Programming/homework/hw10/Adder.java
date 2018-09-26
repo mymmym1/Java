f82acaf281d74c43e67c6b4e7bbaeeecb9d861a8
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2018-09-26 09:10:59 -0400

    Create Adder.java

---
+++ Problem Solving and Object-Oriented Programming/homework/hw10/Adder.java
@@ -0,0 +1,17 @@
+public class Adder {
+    int add(int a, int b) {
+        int sum = 0;
+        sum = a + b;
+        return sum;
+    }
+    double add(double a, double b) {
+        double sum = 0.0;
+        sum = a + b;
+        return sum;
+    }
+    double add(double a, double b, double c) {
+        double sum = 0.0;
+        sum = a + b + c;
+        return sum;
+    }
+}

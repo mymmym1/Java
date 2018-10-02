64fdb6816d3c43558a7f2215b33ad7c5ad798931
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2018-10-02 09:05:14 -0400

    Add files via upload

---
+++ Problem Solving and Object-Oriented Programming/homework/hw14/NegativeBalanceException.java
@@ -0,0 +1,8 @@
+public class NegativeBalanceException extends Exception {
+    public NegativeBalanceException(String message) {
+        super(message);
+    }
+        public NegativeBalanceException () {
+        super();
+    }
+}

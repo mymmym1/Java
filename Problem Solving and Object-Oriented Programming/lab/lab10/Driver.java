82802d3ebc36d506bb898cbcd1bd3aa5a3af4fed
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2018-10-30 12:39:50 -0400

    Create Driver.java

---
+++ Problem Solving and Object-Oriented Programming/lab/lab10/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String[] args){
+        Thread t1 = new A();
+        t1.start();
+        Runnable r = new B();
+        Thread t2 = new Thread(r);
+        t2.start();
+    }
+}
